1f25c7f3dfd3c0179464792ca8b5813e315b297b
Drew Schmidt <schmidt@math.utk.edu>
2014-05-11 19:47:04 -0400

    05.11.14 added some documentation, flips

--- R/papi_flips.r
+++ R/papi_flips.r
@@ -1,15 +1,15 @@
-papi.flops <- function(expr)
+papi.flips <- function(expr)
 {
   papi.init()
   papi.check(3L)
   
-  ret <- .Call("papi_flops_on")
+  ret <- .Call("papi_flips_on")
   if (ret == -1L)
     stop("PAPI failed to initialize hardware counters.\nYour platform may not support floating point operation event.\n")
   
   eval(expr)
   
-  ret <- .Call("papi_flops_off")
+  ret <- .Call("papi_flips_off")
   if (is.integer(ret) && ret == -1L)
     stop("There was a problem recovering the counter information.")
   
@@ -17,10 +17,10 @@ papi.flops <- function(expr)
 }
 
 
-system.flops <- function(expr, gcFirst=TRUE)
+system.flips <- function(expr, gcFirst=TRUE)
 {
   if (gcFirst) 
     gc(FALSE)
   
-  return( papi.flops(expr=expr) )
+  return( papi.flips(expr=expr) )
 }
---
+++ src/flips.c
@@ -0,0 +1,85 @@
+#include "pbdPAPI.h"
+
+#define NUM_EVENTS 3
+
+SEXP papi_flips_on()
+{ 
+  float ireal_time, iproc_time, imflips;
+  long long iflpins;
+  int retval;
+  
+  SEXP RET;
+  PROTECT(RET = allocVector(INTSXP, 1));
+  
+  retval = PAPI_flips(&ireal_time, &iproc_time, &iflpins, &imflips);
+  
+  if (retval != PAPI_OK)
+    INTEGER(RET)[0] = PBD_ERROR;
+  
+  UNPROTECT(1);
+  return RET;
+}
+
+
+
+SEXP papi_flips_off()
+{ 
+  float real_time, proc_time, mflips;
+  long long flpins;
+  int retval;
+  int unpt;
+  long_long values[NUM_EVENTS];
+  
+  SEXP RET, RET_NAMES, REAL_TIME, PROC_TIME, FLPINS, MFLIPS;
+  
+  
+  retval = PAPI_flips(&real_time, &proc_time, &flpins, &mflips);
+  
+  if (retval != PAPI_OK)
+  {    
+    PROTECT(RET = allocVector(INTSXP, 1));
+    INTEGER(RET)[0] = PBD_ERROR;
+    
+    unpt = 1;
+  }
+  else
+  {
+    // Fill return values
+    PROTECT(REAL_TIME = allocVector(REALSXP, 1));
+    PROTECT(PROC_TIME = allocVector(REALSXP, 1));
+    PROTECT(FLPINS = allocVector(REALSXP, 1));
+    PROTECT(MFLIPS = allocVector(REALSXP, 1));
+    
+    REAL(REAL_TIME)[0] = (double) real_time;
+    REAL(PROC_TIME)[0] = (double) proc_time;
+    REAL(FLPINS)[0] = (double) flpins;
+    REAL(MFLIPS)[0] = (double) mflips;
+    
+    // Fill list
+    PROTECT(RET = allocVector(VECSXP, 4));
+    PROTECT(RET_NAMES = allocVector(STRSXP, 4));
+    
+    SET_VECTOR_ELT(RET, 0, REAL_TIME);
+    SET_VECTOR_ELT(RET, 1, PROC_TIME);
+    SET_VECTOR_ELT(RET, 2, FLPINS);
+    SET_VECTOR_ELT(RET, 3, MFLIPS);
+    
+    SET_STRING_ELT(RET_NAMES, 0, mkChar("real_time"));
+    SET_STRING_ELT(RET_NAMES, 1, mkChar("proc_time"));
+    SET_STRING_ELT(RET_NAMES, 2, mkChar("flpins"));
+    SET_STRING_ELT(RET_NAMES, 3, mkChar("mflips"));
+    
+    setAttrib(RET, R_NamesSymbol, RET_NAMES);
+    
+    unpt = 6;
+  }
+  
+  
+  // Turn off counters
+  PAPI_stop_counters(values, NUM_EVENTS);
+  
+  UNPROTECT(unpt);
+  return RET;
+}
+
+
