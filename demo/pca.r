7621c8618b516f87e56a64a1de5ebb8a6e0ad4b0
Drew Schmidt <schmidt@math.utk.edu>
2014-05-11 19:20:48 -0400

    added stats flops demos

---
+++ demo/pca.r
@@ -0,0 +1,21 @@
+library(pbdPAPI)
+
+### Reimplementation of PAPI's FlopsMatrixMatrix.m in R
+
+### ~4MiB
+m <- 10000
+n <- 50
+
+x <- matrix(rnorm(m*n), m, n)
+
+### SVD requires 6mn^2 + 20n^3 flops
+### x %*% svd$v requires 2*m*n^2 flops
+op <- expression(prcomp(x, center=FALSE, scale.=FALSE))
+
+report <- papi.flops(eval(op))
+count <- report$flpops
+theoretical <- 6*m*n^2 + 20*n^3 + 2*m*n^2
+mflops <- report$mflops
+
+data.frame(n=n, measured=count, theoretical=theoretical, difference=count-theoretical, "pct.error"=(1.0 - (theoretical/count))*100, mflops=mflops)
+
---
+++ demo/regression.r
@@ -0,0 +1,18 @@
+library(pbdPAPI)
+
+### ~40MiB
+m <- 50000
+n <- 100
+
+x <- matrix(rnorm(m*n), m, n)
+y <- matrix(rnorm(m), m, 1)
+
+op <- expression(lm.fit(x=x, y=y))
+
+report <- papi.flops(eval(op))
+count <- report$flpops
+theoretical <- 2*n^2*(m-n/3)
+mflops <- report$mflops
+
+data.frame(n=n, measured=count, theoretical=theoretical, difference=count-theoretical, "pct.error"=(1.0 - (theoretical/count))*100, mflops=mflops)
+
