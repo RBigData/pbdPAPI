94d8fdfd896b09d66789740d7f79945d27a35566
wrathematics <wrathematics@gmail.com>
2014-05-27 10:02:17 -0400

    event.init error checking

--- R/papi_event.r
+++ R/papi_event.r
@@ -1,6 +1,26 @@
 papi.event.init <- function(which)
 {
   ret <- .Call("papi_event_counter_init", which) # ret: name to id conversion
+  
+  nas <- is.na(ret)
+  if (any(nas))
+  {
+    if (length(which(nas==TRUE)) == 1L)
+    {
+      event.name <- "Event "
+      tobe.name <- " was "
+      counter.name <- "This counter "
+    }
+    else
+    {
+      event.name <- "Events "
+      tobe.name <- " were "
+      counter.name <- "These counters "
+    }
+    
+    stop(paste(event.name, paste(which[nas], collapse=", "), tobe.name, "undefined.\n", counter.name, "may not be available on your platform.", sep=""))
+  }
+  
   return(ret)
 }
 
@@ -35,7 +55,7 @@ papi.event <- function(expr, which)
   ret <- papi.start(which)
   
   if (!missing("expr"))
-    expr
+    eval(expr)
   
   ret <- papi.stop(which)
   
