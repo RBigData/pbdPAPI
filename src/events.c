f5d47b3c49c4d563b9216f495a6e1498b91303ea
wrathematics <wrathematics@gmail.com>
2014-05-28 15:11:59 -0400

    added copyright notices

--- src/events.c
+++ src/events.c
@@ -1,3 +1,10 @@
+/* This Source Code Form is subject to the terms of the BSD 2-Clause
+ * License. If a copy of the this license was not distributed with this
+ * file, you can obtain one from http://opensource.org/licenses/BSD-2-Clause. */
+
+// Copyright 2014, Heckendorf, Schmidt.  All rights reserved.
+
+
 #include "pbdPAPI.h"
 
 #define NUM_EVENTS LENGTH(which)
--- src/flops.c
+++ src/flops.c
@@ -1,3 +1,10 @@
+/* This Source Code Form is subject to the terms of the BSD 2-Clause
+ * License. If a copy of the this license was not distributed with this
+ * file, you can obtain one from http://opensource.org/licenses/BSD-2-Clause. */
+
+// Copyright 2014, Schmidt.  All rights reserved.
+
+
 #include "pbdPAPI.h"
 
 /***********************************************************************
