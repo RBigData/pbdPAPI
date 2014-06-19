ae5536cde9d8ccb478ff13a2f41ce36df53ce136
wrathematics <wrathematics@gmail.com>
2014-06-19 14:04:29 -0400

    fixed vignette/version build

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PKGVER=`grep "Version:" ../DESCRIPTION | sed -e "s/Version: //"`
-sed -i -e "s/myversion}{.*}/myversion}{${PKGVER}}/" cover.tex
+sed -i -e "s/myversion{.*}/myversion{${PKGVER}}/" pbdPAPI-guide.Rnw
 
 
 rm *.aux *.bbl *.blg *.log *.out *.toc
