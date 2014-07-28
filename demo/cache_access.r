b0744844ab9c6a1f6601be80c1d5253efc6723b1
wrathematics <wrathematics@gmail.com>
2014-07-28 09:52:36 -0400

    moved cache access demo from inline to modern interface

--- demo/cache_access.r
+++ demo/cache_access.r
@@ -1,35 +1,37 @@
 library(pbdPAPI)
-library(inline)
+library(Rcpp)
 
-bad_cache_access <- "
-  int i, j;
-  const int n = INTEGER(n_)[0];
-  Rcpp::NumericMatrix x(n, n);
-  
-  
-  for (i=0; i<n; i++)
-    for (j=0; j<n; j++)
-      x(i, j) = 1.;
-  
-  return x;
-"
-
-good_cache_access <- "
-  int i, j;
-  const int n = INTEGER(n_)[0];
-  Rcpp::NumericMatrix x(n, n);
-  
-  
-  for (j=0; j<n; j++)
+cppFunction("
+  SEXP bad(const int n)
+  {
+    int i, j;
+    Rcpp::NumericMatrix x(n, n);
+    
+    
     for (i=0; i<n; i++)
-      x(i, j) = 1.;
-  
-  return x;
-"
+      for (j=0; j<n; j++)
+        x(i, j) = 1.;
+    
+    return x;
+  }
+")
+
+cppFunction("
+  SEXP good(const int n)
+  {
+    int i, j;
+    Rcpp::NumericMatrix x(n, n);
+    
+    
+    for (j=0; j<n; j++)
+      for (i=0; i<n; i++)
+        x(i, j) = 1.;
+    
+    return x;
+  }
+")
 
 
-bad <- cxxfunction(signature(n_="integer"), body=bad_cache_access, plugin="Rcpp")
-good <- cxxfunction(signature(n_="integer"), body=good_cache_access, plugin="Rcpp")
 
 n <- 10000L
 
