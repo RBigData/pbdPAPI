9e10d451369e9f23ee2263811a71b59cbd98e00c
wrathematics <wrathematics@gmail.com>
2014-08-19 11:04:31 -0400

    cleaned up zzz.r

--- R/zzz.r
+++ R/zzz.r
@@ -1,67 +1,44 @@
-### Lastest load into a package.
+.onLoad <- function(libname, pkgname)
+{
+  ### Intel PCM needs to be loaded before "pbdPAPI.so"
 
-### Export Namespace does not use .First.lib() and .Last.lib(), but use
-### .onLoad() and .onUnload().
-# .First.lib <- function(lib, pkg){
-# } # End of .First.lib().
+  i.lib <- NULL
+  libs <- paste("libs",c("libcipcm","libpcm"),sep="/")
+  full.libs <- system.file(libs,"pbdPAPI")
+  
+  for(i in 1:length(full.libs))
+  {
+    ret <- try(dyn.load(full.libs[i]), silent = TRUE)
+    if(class(ret) != "try-error")
+    {
+      i.lib <- libs[i]
+      break
+    }
+  }
+  
+  library.dynam("pbdPAPI", pkgname, libname)
+  
+  invisible()
+}
 
-# .Last.lib <- function(libpath){
-# } # End of .Last.lib().
 
-.onLoad <- function(libname, pkgname){
-	### Shared library needs to be loaded before "pbdMPI.so"
 
-	i.lib <- NULL
-	libs <- paste("libs",c("libcipcm","libpcm"),sep="/")
-	full.libs <- system.file(libs,"pbdPAPI")
-
-	for(i in 1:length(full.libs)){
-		ret <- try(dyn.load(full.libs[i]), silent = TRUE)
-		if(class(ret) != "try-error"){
-			i.lib <- libs[i]
-			### CRAN doesn't allow this anymore. Use "pkg_initialize" instead.
-			# assign(".__MPI_LIB__", i.libs, envir = .GlobalEnv)
-			break
-		}
-	}
-
-	library.dynam("pbdPAPI", pkgname, libname)
-
-	invisible()
-} # End of .onLoad().
-
-.onUnload <- function(libpath){
+.onUnload <- function(libpath)
+{
   library.dynam.unload("pbdPAPI", libpath)
-
-	libs <- paste("libs",c("libcipcm","libpcm"),sep="/")
-	full.libs <- system.file(libs,"pbdPAPI")
-
+  
+  libs <- paste("libs",c("libcipcm","libpcm"),sep="/")
+  full.libs <- system.file(libs,"pbdPAPI")
+  
     for(i.lib in full.libs){
       ret <- try(dyn.unload(i.lib), silent = TRUE)
     }
   invisible()
-} # End of .onUnload().
+}
 
-.onAttach <- function(libname, pkgname){
-#   if(interactive()){
-#     pbdR.msg <-
-# "
-#     pbdR is mainly designed for batch environment or in SPMD programming.
-#     Please utilize High Performance Computing Resource or consider an
-#     allociation via ``http://www.nics.tennessee.edu/getting-an-allocation''
-#     with XSEDE at ``http://www.xsede.org/overview''.
-# "
-#   } else{
-#     pbdR.msg <-
-# "
-#     High Performance Computing Resource is available via
-#     ``http://www.nics.tennessee.edu/getting-an-allocation''
-#     with XSEDE at ``http://www.xsede.org/overview''.
-# "
-#   }
-#   if(spmd.comm.rank(0L) == 0){
-#     packageStartupMessage(pbdR.msg)
-#   }
-  #invisible()
+
+
+.onAttach <- function(libname, pkgname)
+{
   papi.avail.internal()
-} # End of .onAttach().
+}
