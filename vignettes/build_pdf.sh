0ea0335e3ee190c55afbb9bbaab4bc0a01e02084
Drew Schmidt <schmidt@math.utk.edu>
2016-01-24 10:26:34 -0500

    update vignette

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -3,6 +3,9 @@
 PKGVER=`grep "Version:" ../DESCRIPTION | sed -e "s/Version: //"`
 sed -i -e "s/myversion{.*}/myversion{${PKGVER}}/" pbdPAPI-guide.Rnw
 
+cd cover
+./build_cover.sh
+cd ..
 
 rm *.aux *.bbl *.blg *.log *.out *.toc
 pdflatex pbdPAPI-guide.Rnw
@@ -12,5 +15,5 @@ pdflatex pbdPAPI-guide.Rnw
 Rscript -e "tools::compactPDF('pbdPAPI-guide.pdf', gs_quality='ebook')"
 rm *.aux *.bbl *.blg *.log *.out *.toc *.dvi
 
-mv -f *.pdf ../inst/doc/
-cp -f *.Rnw ../inst/doc/
+#mv -f *.pdf ../inst/doc/
+#cp -f *.Rnw ../inst/doc/
--- vignettes/cover/build_cover.sh
+++ vignettes/cover/build_cover.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+PKGVER=`grep "Version:" ../../DESCRIPTION | sed -e "s/Version: //"`
+sed -i -e "s/thispackageversion}{.*}/thispackageversion}{${PKGVER}}/" cover.tex
+
 xelatex cover.tex
 xelatex cover.tex
 Rscript -e "tools::compactPDF('.', gs_quality='ebook')"
-rm *.aux *.bbl *.blg *.log *.out *.toc *.idx *.lof *.lot *.ind *.ilg
+rm -rf *.dvi *.aux *.bbl *.blg *.log *.out *.toc *.idx *.lof *.lot *.ind *.ilg
+
