3dedd9edfa69380d71295725f5f79a1dad014348
wrathematics <wrathematics@gmail.com>
2016-03-16 18:15:52 -0400

    add flopsbench()

---
+++ R/flopsbench.r
@@ -0,0 +1,72 @@
+#' flopsbench
+#' 
+#' A benchmarker for measuring flops (floating point operations per
+#' second) of one or more functions.
+#' 
+#' @param data
+#' A list of data for the provided functions.
+#' @param ...
+#' Expressions to be benchmarked.
+#' 
+#' @return
+#' A dataframe (class "flopsbench") containng the timings and some
+#' basic summary statistics.
+#' 
+#' @examples
+#' \dontrun{
+#' data <- list(rnorm(1e4), rnorm(2e4), rnorm(3e4))
+#' flopsbench(sqrt, exp, data=data)
+#' }
+#' 
+#' @export
+flopsbench <- function(..., data)
+{
+  l <- list(...)
+  len <- length(l)
+  if (len == 0)
+    stop("No expressions")
+  
+  datalen <- length(data)
+  
+  
+  args <- match.call()[-1]
+  names <- names(args)
+  if (is.null(names))
+    argnames <- as.character(args)
+  else
+  {
+    keep <- names(args) != "data"
+    args <- args[keep]
+    argnames <- names[keep]
+    charargs <- as.character(args)
+    
+    argnames <- sapply(1:len, function(i) if (argnames[i] == "") charargs[i] else argnames[i])
+  }
+  
+  template <- system.flops(NULL)
+  colnames <- names(template)
+  ret <- lapply(1:len, function(.) {tmp <- matrix(0, nrow=datalen, ncol=4); colnames(tmp) <- colnames; tmp})
+  names(ret) <- argnames
+  
+  for (i in 1:len)
+  {
+    for (j in 1:datalen)
+    {
+      tmp <- system.flops(expr=eval(args[[i]]))
+      ret[[i]][j, ] <- as.numeric(tmp)
+    }
+  }
+  
+  
+  n <- sapply(data, NROW) * sapply(data, NCOL)
+  ret <- lapply(ret, function(x) cbind(x, n))
+  
+  # means <- do.call(rbind, lapply(ret, colMeans))
+  # ret$summarystats <- means
+  
+  # type <- "FLOP/s"
+  # ret$type <- type
+  class(ret) <- "flopsbench"
+  
+  return(ret)
+}
